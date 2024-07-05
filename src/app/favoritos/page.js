e6bd2592c266a1949f82fc3b412dfbebe8d0c1fb
12Gustavo21 <gustavo927318@gmail.com>
2024-07-05 04:41:49 -0300

    style: update style variables and scroll bar

--- src/app/favoritos/page.js
+++ src/app/favoritos/page.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 //Components
 import HandleViewDetails from "../assets/components/viewDetails";
+import useSpecificCar from "../assets/components/getSpecificCar";
 
 //Styles
 import styles from "./page.module.sass";
@@ -23,6 +24,15 @@ export default function Page() {
     const favoritos = JSON.parse(localStorage.getItem("favoritos"));
     setFavoritos(favoritos);
   }, []);
+
+  function removeCar(id) {
+    const favoritos = JSON.parse(localStorage.getItem("favoritos"));
+    const updatedFavoritos = favoritos.filter((car) => car.id !== id);
+    localStorage.setItem("favoritos", JSON.stringify(updatedFavoritos));
+    alert("Carro removido dos favoritos");
+    const newFavoritos = JSON.parse(localStorage.getItem("favoritos"));
+    setFavoritos(newFavoritos);
+  }
 
   useEffect(() => {
     AOS.init({
@@ -47,6 +57,19 @@ export default function Page() {
                   data-aos-duration="2400"
                 >
                   <div className={styles.img__container}>
+                    <div
+                      onClick={() => removeCar(car.id)}
+                      className={styles.remove__favorite}
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 64 64"
+                      >
+                        <path d="M47 5c-6.5 0-12.9 4.2-15 10-2.1-5.8-8.5-10-15-10A15 15 0 0 0 2 20c0 13 11 26 30 39 19-13 30-26 30-39A15 15 0 0 0 47 5z"></path>
+                      </svg>
+                    </div>
                     <div className={styles.img__wrapper}>
                       <figure className={styles.img__skew}>
                         <Image
